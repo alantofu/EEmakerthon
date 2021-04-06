e5c589156a7e4b0c5fb650a452a8c0c1ae73110a
Alantofu <alantofu99@gmail.com>
2021-04-07 02:31:06 +0800

    add firebase code

--- webscrapping.py
+++ webscrapping.py
@@ -4,16 +4,19 @@ from bs4 import BeautifulSoup
 import firebase_admin
 from firebase_admin import credentials
 import re
+import os 
 
-startdate = date(2021, 4, 6)   # start date
-enddate = date(2021, 4, 7)   # end date
+cred = credentials.Certificate(os.path.dirname(os.path.realpath(__file__))+"\covideemakerthon-firebase-adminsdk-dc85d-8b79d2065d.json")
+firebase_admin.initialize_app(cred)
+startdate = date(2021, 4, 5)   # start date
+enddate = date(2021, 4, 6)   # end date
 malaymonth = ["januari", "februari", "mac", "april", "may", "jun","julai","ogos","september","oktober","november","disember"]
 delta = enddate - startdate
 for i in range(delta.days + 1):
     x = startdate + timedelta(days=i)
-    value = str(x.month) if len(str(x.month))>1 else "0" +str(x.month)
-    url = "https://kpkesihatan.com/"+x.strftime("%Y")+"/"+ value +"/"+x.strftime("%d")+"/kenyataan-akhbar-kpk-"+str(x.day)+"-"+malaymonth[x.month-1]+"-"+x.strftime("%Y")+"-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/"
+    url = "https://kpkesihatan.com/"+x.strftime("%Y")+"/"+ str(x.month) +"/"+x.strftime("%d")+"/kenyataan-akhbar-kpk-"+str(x.day)+"-"+malaymonth[x.month-1]+"-"+x.strftime("%Y")+"-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/"
     response = requests.get(url)
+    print(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     f = soup.findAll("figure",{"class":"wp-block-table"})
     t = f[1].find("table")
@@ -26,4 +29,7 @@ for i in range(delta.days + 1):
             tempstring = re.sub(r'\([^)]*\)', '', tempstring)
             rec.append(tempstring)
         db.append(rec)
+
+
+
     
